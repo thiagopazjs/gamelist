a742de86036185632c8b18eb93b79af09bc17d78
Thiago Paz <tpazdev@gmail.com>
2025-05-19 12:16:40 -0300

    Project created

---
+++ src/test/java/com/brildev/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brildev.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
