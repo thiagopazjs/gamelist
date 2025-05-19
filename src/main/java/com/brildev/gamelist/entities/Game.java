f102a9f8d96e12214df0606585314f509009dd8d
Thiago Paz <tpazdev@gmail.com>
2025-05-19 13:39:42 -0300

    game seed

--- src/main/java/com/brildev/gamelist/entities/Game.java
+++ src/main/java/com/brildev/gamelist/entities/Game.java
@@ -14,12 +14,15 @@ public class Game {
     private Long id;
     private String title;
 
-    @Column(name = "name_year")
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platform;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Long getId() {
@@ -62,6 +65,14 @@ public class Game {
         this.platform = platform;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getImgUrl() {
         return imgUrl;
     }
@@ -98,12 +109,13 @@ public class Game {
         return Objects.hashCode(id);
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platform, Double score,String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platform = platform;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
